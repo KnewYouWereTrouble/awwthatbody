78b8036390311ca0330566fb28bce450eb7a2799
desmondjiosg <desmond@jio.sg>
2016-08-01 13:32:16 +0800

    fixed login ui

--- views/components/login/Login.jsx
+++ views/components/login/Login.jsx
@@ -23,10 +23,19 @@ export default React.createClass({
         return (
             <div className="container login-wrapper">
                 <div className="row">
+
+
                     <div className="col s4 offset-s4">
                         <h3 className="condensed light">Aww That Body</h3>
-                        {this.state.loginForm ? <LoginForm toggleLogin={this.toggleLogin} /> :
-                        <SignupForm toggleLogin={this.toggleLogin}/> }
+                        <img className="loginboy1" src="/images/loginboy2.png" height="250"/>
+
+                        {this.state.loginForm ?
+                        <LoginForm toggleLogin={this.toggleLogin} />
+                        :
+                        <SignupForm toggleLogin={this.toggleLogin} />
+                        }
+
+                        <img className="loginboy2" src="/images/loginboy1.png" height="250"/>
                     </div>
                 </div>
             </div>
